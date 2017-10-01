d9fe9ef706c366ca25994a9ea087b351b2f894fa
Sanya <sanya.shaikh82@gmail.com>
2017-10-02 01:19:43 +0530

    include train_csv file

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
 import pandas
-titanic=pandas.read_csv('C:\\Users\FAIYAZ\Downloads\\train.csv')
+#titanic=pandas.read_csv('C:\\Users\FAIYAZ\Downloads\\train.csv')
+titanic=pandas.read_csv('data/train.csv')
 print(titanic)
